7ef233c9b4ec439553526bfb3dedb28dedff6ca7
Jennifer Logrell <jennifer.logrell@hotmail.com>
2024-02-12 21:16:00 +0100

    hämtat adress från api

---
+++ src/models/ContactClass.ts
@@ -0,0 +1,10 @@
+export class ContactClass {
+    constructor(    
+        public restaurantID: string,
+        public name: string,
+        public address: string,
+        public zip: string,
+        public city: string,
+        )
+        {}
+    }
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,9 +1,43 @@
+import { ContactClass } from "../models/ContactClass";
+import axios from "axios";
+import { resturantId } from "./Home";
+import { useState } from "react";
+import "../Style/contact.scss";
+
 export const Contact = () => {
+  const [contact, setContact] = useState<ContactClass>();
+
+  if (!contact) {
+    axios
+      .get<ContactClass[]>(
+        "https://school-restaurant-api.azurewebsites.net/restaurant/" +
+          resturantId
+      )
+      .then((response) => {
+        setContact(response.data[0]);
+      });
+  }
   return (
+    <>
+      <div>
+        <p className="name">{contact?.name}</p>
+        <p className="address">
+          {contact?.address}
+        <p className="address">  {contact?.zip}
+          {contact?.city}</p>
+        </p>
+      </div>
+    </>
+  );
+};
+
+/*  return (
     <>
       <h4>Kontakta oss</h4>
       <div>
+        <p>
 
+        </p>
       </div>
       <div>
         <input type="text" placeholder="Namn"/>
@@ -12,4 +46,4 @@ export const Contact = () => {
       </div>
     </>
   );
-};
+; */
