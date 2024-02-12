2263ad026ae442071e512fb17dff7c68fb1b2718
Rebecca Hansson <rebeccahansson2016@gmail.com>
2024-02-12 10:34:30 +0100

    deleted app.css

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import { RouterProvider } from "react-router";
 import { Router } from "./Router.tsx";
+import "./main.css";
 
 // const resturantId = import.meta.env.VITE_RESTURANTID;
 
