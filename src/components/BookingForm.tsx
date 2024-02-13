28a7467976738e92c0d231743148ce5abc74a401
Sanna Siljebäck <sannasiljeback@gmail.com>
2024-02-13 14:52:25 +0100

    started axios post

--- src/components/BookingForm.tsx
+++ src/components/BookingForm.tsx
@@ -1,6 +1,15 @@
 import { FormEvent, useState } from "react";
 
 export const BookingForm = () => {
+
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhoneNumber] = useState("");
+  const [date, setDate] = useState("");
+  const [time, setTime] = useState("");
+
+
   const handleSubmit = (e: FormEvent) => {
     e.preventDefault();
   };
@@ -17,19 +26,27 @@ export const BookingForm = () => {
     setIsChecked(!isChecked);
   };
 
+  const handleBooking = () => {
+
+
+  };
+
+
+
+
   return (
     <>
       <form onSubmit={handleSubmit}>
         <label htmlFor="name">Namn:</label>
-        <input type="text" placeholder="Förnamn" />
-        <input type="text" placeholder="Efternamn" />
+        <input type="text" placeholder="Förnamn" value={firstName}/>
+        <input type="text" placeholder="Efternamn" value={lastName} />
 
         <label htmlFor="mail">Email:</label>
-        <input type="text"/>
+        <input type="text" value={email}/>
 
         <label htmlFor="phoneNumber">Telefonnummer:</label>
-        <input type="text"/>
-        <button>Boka</button>
+        <input type="text" value={phoneNumber}/>
+        
 
         <label htmlFor="personQuantity">Antal personer</label>
         <select id="personQuantity" value={persons} onChange={handlePersons}>
@@ -40,10 +57,15 @@ export const BookingForm = () => {
           ))}
         </select>
 
-
       
-        <label htmlFor="chooseDate">Välj datum:</label>
-        <input type="date" id="chooseDate" name="chooseDate" />
+        <label htmlFor="chooseDateTime">Välj datum och tid:</label>
+        <input type="date" id="chooseDateTime" name="chooseDate" value={date} />
+      
+        <select id="chooseDateTime" value={time}>
+          <option value="">Välj tid</option>
+          <option value="18:00">18:00</option>
+          <option value="21:00">21:00</option>
+        </select>
       
 
 
@@ -51,6 +73,8 @@ export const BookingForm = () => {
             <input type="checkbox" checked={isChecked} onChange={handleCheckbox} />
             <span>Jag godkänner användarvillkoren</span>
         </label>
+
+        <button onClick={handleBooking}>Boka</button>
       </form>
     </>
   );
