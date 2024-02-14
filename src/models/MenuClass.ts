02ad3125d5dfcf81af471ea7c9752030aa9f4949
Jennifer Logrell <jennifer.logrell@hotmail.com>
2024-02-14 16:03:54 +0100

    Menu done (ish), no styling

---
+++ src/models/MenuClass.ts
@@ -0,0 +1,11 @@
+export class Food {
+    constructor (
+    public name: string,
+    public dishOne: string,
+    public dishTwo: string,
+    public drink: string,
+    public dishThree: string,
+    public price: number
+    )
+    {}
+}
