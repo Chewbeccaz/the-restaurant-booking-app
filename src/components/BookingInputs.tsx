2844ebe17ec3d469d6353c448e725661ce393acf
Sanna Siljebäck <sannasiljeback@gmail.com>
2024-02-15 23:23:13 +0100

    started booking input component

--- src/components/BookingInputs.tsx
+++ src/components/BookingInputs.tsx
@@ -1,7 +1,23 @@
-export const BookingInputs = () => {
+interface IBookingInputs {
+    label: string;
+    name: string;
+    type: string;
+    value: string;
+    onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+}
+
+
+export const BookingInputs: React.FC<IBookingInputs> = ({label, name, type, value, onChange}) => {
     return (
         <>
-        
+        <label htmlFor="name">{label}</label>
+        <input
+        id={name}
+        name={name}
+        type={type}
+        value={value}
+        onChange={onChange}
+        />  
         </>
     );
 };
