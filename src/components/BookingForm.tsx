604e5862cfba490096a6679aee3f27fe51e2d3e4
Sanna Siljebäck <sannasiljeback@gmail.com>
2024-02-16 18:33:10 +0100

    fixed current date

--- src/components/BookingForm.tsx
+++ src/components/BookingForm.tsx
@@ -9,15 +9,6 @@ import { getCurrentDate } from "./CurrentDate";
 
 import { makeBooking } from "../services/BookingService";
 
-const getCurrentDate = () => {
-  const currentDate = new Date();
-  const year = currentDate.getFullYear();
-  const month = (currentDate.getMonth() + 1).toString().padStart(2, "0");
-  const day = currentDate.getDate().toString().padStart(2, "0");
-
-  return `${year}-${month}-${day}`;
-};
-
 
 export const BookingForm = () => {
   const [firstName, setFirstName] = useState("");
